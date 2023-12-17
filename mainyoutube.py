8a7eee04a74c10f0b7aa163fab9c18edcb05acdf
BertrandAtangana <batangana@gmail.com>
2023-12-17 15:50:10 +0100

    first version

--- mainyoutube.py
+++ mainyoutube.py
@@ -4,9 +4,19 @@
 
 from pytube import YouTube
 
-url = "https://www.youtube.com/watch?v=9bZkp7q19f0"
+url = "https://www.youtube.com/watch?v=D19soDJCIjY"
+
+
+def on_download_progress(stream, chunk, bytes_remaining):
+    bytes_downloaded = stream.filesize - bytes_remaining
+    percent =bytes_downloaded*100/stream.filesize
+    print(f"Progression du téléchargement{int(percent)}%")
+
 
 youtube_video = YouTube(url)
+
+youtube_video.register_on_progress_callback(on_download_progress)
+
 print("TITRE: " + youtube_video.title)
 print("NB VUES: ", youtube_video.views)
 
